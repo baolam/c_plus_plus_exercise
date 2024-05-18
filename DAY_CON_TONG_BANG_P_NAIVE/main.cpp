d7d9b6ed58fcefb9635d29ef8e4ab451498bed30
baolam <lam01662052827@gmail.com>
2024-05-18 18:48:03 +0700

    Updating

--- DAY_CON_TONG_BANG_P_NAIVE/main.cpp
+++ DAY_CON_TONG_BANG_P_NAIVE/main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define maxn 1000002
 
 using namespace std;
+typedef long long ll;
+
+ll n, p;
+ll a[maxn], s[maxn];
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> p;
+    s[0] = 0;
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i], s[i] = s[i - 1] + a[i];
+        for (int j = 1; j <= i; j++)
+            if (s[i] - s[j - 1] == p)
+                cnt++;
+    }
+    cout << cnt;
     return 0;
 }
