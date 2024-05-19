7f8a46bc3bea18f2880be454fa383ac6b4a38da1
baolam <lam01662052827@gmail.com>
2024-05-19 20:52:26 +0700

    Updating

---
+++ GENERATING_SUBSET_METHOD_1/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define maxn 50
+
+using namespace std;
+typedef long long ll;
+
+ll n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < (1 << n); i++)
+    {
+        // Xử lí subset
+    }
+    return 0;
+}
