ab11c4e8f1457607dc82a80ab1e21be898fd3cf4
baolam <lam01662052827@gmail.com>
2024-05-31 20:20:10 +0700

    Updating

---
+++ BUS/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define maxn 200002
+
+using namespace std;
+typedef long long ll;
+
+ll n, m;
+ll sum, gtln = -1;
+ll t[maxn], e[maxn];
+
+bool Check(ll time)
+{
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    freopen("VAO.INP", "r", stdin);
+    freopen("RA.OUT", "w", stdout);
+    ll k, idx = 1;
+    cin >> n >> m;
+    for (ll i = 1; i <= n; i++)
+    {
+        cin >> t[i] >> k;
+        while(k != 0)
+        {
+            cin >> e[cnt];
+            gtln = max(gtln, e[cnt]);
+            cnt++;
+            k--;
+        }
+    }
+    return 0;
+}
