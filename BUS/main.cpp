cbdb42bd4b4592cc8c1b0bad7461b7f0f6b3e83f
baolam <lam01662052827@gmail.com>
2024-05-31 20:52:07 +0700

    Updating

--- BUS/main.cpp
+++ BUS/main.cpp
@@ -6,11 +6,19 @@ typedef long long ll;
 
 ll n, m;
 ll sum, gtln = -1;
-ll t[maxn], e[maxn];
+ll t[maxn];
+vector<vector<ll>> e;
 
 bool Check(ll time)
 {
-
+    ll cnt = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        time += t[i];
+        ll tmp = lower_bound(e[i].begin(), e[i].end(), time) - e[i].begin();
+        cout << tmp << ' ';
+    }
+    return false;
 }
 
 int main()
@@ -19,21 +27,22 @@ int main()
     cin.tie(NULL); cout.tie(NULL);
     freopen("VAO.INP", "r", stdin);
     freopen("RA.OUT", "w", stdout);
-    ll k, cnt = 1;
+    ll k, x;
     cin >> n >> m;
     for (ll i = 1; i <= n; i++)
     {
         cin >> t[i] >> k;
+        vector<ll> tmp;
         for (ll j = 1; j <= k; j++)
         {
-            cin >> e[cnt];
-            cnt++; gtln = max(gtln, e[cnt]);
+           cin >> x;
+           sum += x;
+           gtln = max(gtln, x);
+           tmp.push_back(x);
         }
+        sort(tmp.begin(), tmp.end());
+        e.push_back(tmp);
     }
-    cnt--;
-    sort(e + 1, e + cnt);
-    for (ll i = 1; i <= cnt; i++)
-        cout << e[i] << ' ';
-    cout << '\n';
+    Check(1);
     return 0;
 }
