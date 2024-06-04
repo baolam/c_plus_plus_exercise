44d182ed4083a1bfc1fb66b8e3ec7abada92ea36
baolam <lam01662052827@gmail.com>
2024-06-04 17:48:42 +0700

    Updating

--- LIS_RECURSIVE_WITH_MEM/main.cpp
+++ LIS_RECURSIVE_WITH_MEM/main.cpp
@@ -5,21 +5,27 @@ using namespace std;
 typedef long long ll;
 
 ll n;
-ll a[maxn];
+bool ok[maxn];
+ll a[maxn], value[maxn];
 
 ll solve(ll x)
 {
+    if (ok[x])
+        return value[x];
     ll bst = 1;
     for (ll i = 1; i < x; i++)
     {
         if (a[x] > a[i])
             bst = max(bst, solve(i) + 1);
     }
+    ok[x] = true;
+    value[x] = bst;
     return bst;
 }
 
 int main()
 {
+    memset(ok, false, sizeof(ok));
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     freopen("VAO.INP", "r", stdin);
