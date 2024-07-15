91bb6411e5b5ad998b14c10ff6572212eec9ca70
baolam <lam01662052827@gmail.com>
2024-07-15 19:05:58 +0700

    Updating

--- LINKED_LIST_EXCER/main.cpp
+++ LINKED_LIST_EXCER/main.cpp
@@ -3,23 +3,67 @@
 using namespace std;
 typedef long long ll;
 
-struct ListNode {
+struct node {
     int data;
-    ListNode *link;
+    node *link;
 };
+
+int x;
+node *head = NULL;
+
+node* createNode(int data)
+{
+    node *temp = new node();
+    temp->data = data;
+    temp->link = NULL;
+    return temp;
+}
+
+node* addAtEnd(node *ptr, int data)
+{
+    node *temp = createNode(data);
+    ptr->link = temp;
+    return temp;
+}
+
+node* addAtBegining(node *ptr, int data)
+{
+    node *temp = createNode(data);
+    temp->link = ptr;
+    return temp;
+}
+
+void PrintLinkedList()
+{
+    node *ptr = head;
+    while (ptr != NULL)
+    {
+        cout << ptr->data << ' ';
+        ptr = ptr->link;
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    ListNode *head = new ListNode();
-    head->data = 45;
-    head->link = NULL;
-
-    ListNode *current = new ListNode();
-    current->data = 98;
-    current->link = NULL;
-
-    head->link = current;
+    freopen("VAO.INP", "r", stdin);
+    //freopen("RA.OUT", "w", stdout);
+    node *ptr;
+    while (x != -1)
+    {
+        cin >> x;
+        if (x == -1)
+            break;
+        if (head == NULL)
+        {
+            head = createNode(x);
+            ptr = head;
+        }
+        else
+            ptr = addAtEnd(ptr, x);
+    }
+    head = addAtBegining(head, 234);
+    PrintLinkedList();
     return 0;
 }
