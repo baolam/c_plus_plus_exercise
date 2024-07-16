1aabd1feb109345d0b6b264910b4f245670ca226
baolam <lam01662052827@gmail.com>
2024-07-16 22:03:22 +0700

    Updating

---
+++ DAY_CON_TANG_EXCER/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define maxn 1002
+
+using namespace std;
+typedef long long ll;
+
+ll n;
+ll a[maxn];
+///
+ll cs[maxn];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    freopen("VAO.INP", "r", stdin);
+    freopen("RA.OUT", "w", stdout);
+    cin >> n;
+    for (ll i = 1; i <= n; i++)
+        cin >> a[i];
+
+    return 0;
+}
