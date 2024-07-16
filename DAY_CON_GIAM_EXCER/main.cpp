fee6b95dad54fc59dd94aee37710a56cfdf79ed6
baolam <lam01662052827@gmail.com>
2024-07-16 22:21:03 +0700

    Updating

---
+++ DAY_CON_GIAM_EXCER/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define maxn 1001
+
+using namespace std;
+typedef long long ll;
+
+ll n;
+ll a[maxn], cs[maxn];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    freopen("VAO.INP", "r", stdin);
+    //freopen("RA.OUT", "w", stdout);
+    cin >> n;
+    for (ll i = 1; i <= n; i++)
+        cin >> a[i];
+    cs[1] = 1;
+    ll k = 1;
+    for (ll i = 2; i <= n; i++)
+    {
+        if (a[i] > a[cs[1]])
+            cs[1] = i;
+        else
+        {
+            if (a[i] < a[cs[k]])
+            {
+                k++;
+                cs[k] = i;
+            }
+            else
+            {
+                ll l = 1, r = k;
+                while (l < r)
+                {
+                    ll m = l + (r - l) / 2;
+                    if (a[i] < a[cs[k]])
+                        l = m + 1;
+                    else r = m;
+                }
+                cs[l] = i;
+            }
+        }
+    }
+    cout << k << '\n';
+    for (ll i = 1; i <= k; i++)
+        cout << a[cs[i]] << ' ';
+    return 0;
+}
