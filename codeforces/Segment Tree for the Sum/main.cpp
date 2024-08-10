932afcf9af38fb5b5cea203ddb00da4ea7fbf1b2
baolam <lam01662052827@gmail.com>
2024-08-10 15:13:23 +0700

    updating

---
+++ codeforces/Segment Tree for the Sum/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const int MAXN = 1e5 + 1;
+int a[MAXN], segment_tree[4 * MAXN];
+int n;
+
+void build(int id, int l, int r)
+{
+    if (l == r)
+    {
+        segment_tree[id] = a[l];
+        return;
+    }
+    int m = (l + r) / 2;
+    build(2 * id, l, m);
+    build(2 * id + 1, m + 1, r);
+    segment_tree[id] = segment_tree[2 * id] + segment_tree[2 * id + 1];
+}
+
+void update(int i, int v, int id, int l, int r)
+{
+    if (i < l || r < i) return;
+    if (l == r)
+    {
+        segment_tree[id] = v;
+        return;
+    }
+    int m = (l + r) / 2;
+    if (i <= m) update(i, v, 2 * id, l, m);
+    else update(i, v, 2 * id + 1, m + 1, r);
+    segment_tree[id] = segment_tree[2 * id] + segment_tree[2 * id + 1];
+}
+
+ll sum(int u, int v, int id, int l, int r)
+{
+    if (u > r || v < l) return 0;
+    if (u >= l && v <= r) return segment_tree[id];
+    int m = (l + r) / 2;
+    ll s1 = sum(u, v, 2 * id, l, m);
+    ll s2 = sum(u, v, 2 * id + 1, m + 1, r);
+    return s1 + s2;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    build(1, 1, n);
+    while (m)
+    {
+        int op, x1, x2;
+        cin >> op >> x1 >> x2;
+        if (op == 1)
+            update(x1 + 1, x2, 1, 1, n);
+        else cout << sum(x1 + 1, x2 + 1, 1, 1, n) << '\n';
+        m--;
+    }
+    return 0;
+}
