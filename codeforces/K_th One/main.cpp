776b1d1edd4a1652495733b392a050d948d32bfe
baolam <lam01662052827@gmail.com>
2024-08-13 14:53:28 +0700

    updating

--- codeforces/K_th One/main.cpp
+++ codeforces/K_th One/main.cpp
@@ -5,33 +5,13 @@ typedef long long ll;
 
 const int MAXN = 1e5 + 1;
 int n, m;
-int a[MAXN];
+int a[MAXN], st[4 * MAXN];
 
-struct SegmentTree
-{
-    int idx = -1;
-    int sum = 1;
-
-    void assgn(int i)
-    {
-        idx = i;
-        sum = a[i];
-    }
-};
-
-SegmentTree operator+(SegmentTree x1, SegmentTree x2)
-{
-    SegmentTree out;
-    out.sum = x1.sum + x2.sum;
-    return out;
-}
-
-SegmentTree st[4 * MAXN];
 void build(int id, int l, int r)
 {
     if (l == r)
     {
-        st[id].assgn(l);
+        st[id] = a[l];
         return;
     }
     int mid = (l + r) / 2;
@@ -45,40 +25,40 @@ void update(int i, int id, int l, int r)
     if (i < l || i > r) return;
     if (l == r)
     {
-        st[id].sum = ! st[id].sum;
+        st[id] = st[id] == 1 ? 0 : 1;
+        cout << "Update " << i << ' ' << st[id] << '\n';
         return;
     }
     int mid = (l + r) / 2;
     update(i, 2 * id, l, mid);
-    update(i, 2 * id + 1, mid + 1, r);
+    update(i, 2 * id, mid + 1, r);
     st[id] = st[2 * id] + st[2 * id + 1];
 }
 
-int query(int k, int id)
+int query(int k, int id, int l, int r)
 {
-    if (k == 0)
-        return st[id].idx;
-    if (k < st[id].sum)
-        return query(k, 2 * id);
-    return query(k - st[id].sum, 2 * id + 1);
-};
+    if (l == r)
+        return l;
+    int mid = (l + r) / 2;
+    if (k <= st[2 * id])
+        return query(k, 2 * id, l, mid);
+    return query(k - st[2 * id], 2 * id + 1, mid + 1, r);
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    freopen("VAO.INP", "r", stdin);
-    //freopen("RA.OUT", "w", stdout);
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
     build(1, 1, n);
-    int op, x;
+    int op, x1;
     while (m)
     {
-        cin >> op >> x;
-        if (op == 1) update(x + 1, 1, 1, n);
-        else cout << query(x + 1, 1) << '\n';
+        cin >> op >> x1;
+        if (op == 1) update(x1 + 1, 1, 1, n);
+        else cout << query(x1 + 1, 1, 1, n) << '\n';
         m--;
     }
     return 0;
