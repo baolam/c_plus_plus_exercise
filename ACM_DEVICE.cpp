efb8a5c39761237a8138a0c5ae02e6c12f4b5edc
baolam <lam01662052827@gmail.com>
2024-08-15 23:08:46 +0700

    Updating

--- ACM_DEVICE.cpp
+++ ACM_DEVICE.cpp
@@ -6,6 +6,8 @@ typedef long long ll;
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     return 0;
 }
--- BINARY_TREE.cpp
+++ BINARY_TREE.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 typedef long long ll;
 
-class Node {
+class Node
+{
 public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
     Node(int val)
     {
@@ -20,9 +21,10 @@ public:
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    Node* root = new Node(1);
+    Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
