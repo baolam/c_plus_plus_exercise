111746fe81da179bde2b7b329eb27cd3425f9681
baolam <lam01662052827@gmail.com>
2024-08-16 13:06:15 +0700

    Updating

--- #codeforces/Ropes/ROPES.cpp
+++ #codeforces/Ropes/ROPES.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-// #define HAS_FILE
+#define HAS_FILE
 
 #ifdef HAS_FILE
 #define FILE_NAME "ROPES"
@@ -12,7 +12,7 @@ typedef long long ll;
 
 const int MAXN = 1e4 + 1;
 int n, k;
-ll sum = 0;
+int gtln = INT_MIN;
 int a[MAXN];
 
 bool f(double x)
@@ -26,7 +26,7 @@ bool f(double x)
 double bSearch()
 {
   double l = 0;
-  double r = sum / k;
+  double r = gtln;
   while (r - l > 1e-6)
   {
     double x = (l + r) / 2;
@@ -51,7 +51,7 @@ int main()
   for (int i = 1; i <= n; i++)
   {
     cin >> a[i];
-    sum += a[i];
+    gtln = max(gtln, a[i]);
   }
   cout << bSearch() << '\n';
   return 0;
