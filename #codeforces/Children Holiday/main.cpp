004ea08f0b73306fe29a28c71535f08a93dfd3ca
baolam <lam01662052827@gmail.com>
2024-08-18 15:35:07 +0700

    Updating

---
+++ #codeforces/Children Holiday/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define HAS_FILE
+
+#ifdef HAS_FILE
+#define FILE_NAME "CH"
+#define READ FILE_NAME ".INP"
+#define WRITE FILE_NAME ".OUT"
+#endif
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+#ifdef HAS_FILE
+  freopen(READ, "r", stdin);
+  freopen(WRITE, "w", stdout);
+#endif
+  return 0;
+}
