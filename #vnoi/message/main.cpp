e3e6531b4cff14962bbea2e8be6d0b66da501bbc
baolam <lam01662052827@gmail.com>
2024-12-17 14:48:44 +0700

    message updating

--- #vnoi/message/main.cpp
+++ #vnoi/message/main.cpp
@@ -5,68 +5,29 @@ using namespace std;
 typedef long long ll;
 
 int n, m;
-bool visited[maxn];
-vector<int> V[maxn];
-
-void dfs(int x)
-{
-  visited[x] = false;
-
-  stack<int> temp;
-  temp.push(x);
-
-  while (!temp.empty())
-  {
-    int u = temp.top();
-    temp.pop();
-    for (int i = 0; i < V[u].size(); i++)
-    {
-      int v = V[u][i];
-      if (visited[v])
-      {
-        visited[v] = false;
-        temp.push(v);
-      }
-    }
-  }
-}
-
-int solve_at(int x)
-{
-  memset(visited, true, sizeof(visited));
-
-  int ans = 1;
-  dfs(x);
-
-  for (int i = 1; i <= n; i++)
-  {
-    if (visited[i])
-    {
-      dfs(i);
-      ans++;
-    }
-  }
-
-  return ans;
-}
+int deg_in[maxn];
 
 int main()
 {
+  memset(deg_in, 0, sizeof(deg_in));
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  // freopen("TEST.INP", "r", stdin);
+  freopen("TEST.INP", "r", stdin);
   cin >> n >> m;
   int u, v;
   for (int i = 1; i <= m; i++)
   {
     cin >> u >> v;
-    V[u].push_back(v);
+    deg_in[v]++;
   }
 
-  int ans = INT_MAX;
+  int ans = 0;
   for (int i = 1; i <= n; i++)
-    ans = min(ans, solve_at(i));
+  {
+    if (deg_in[i] == 0)
+      ans++;
+  }
   cout << ans;
   return 0;
 }
