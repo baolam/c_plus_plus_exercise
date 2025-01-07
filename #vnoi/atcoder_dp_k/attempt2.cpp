c0d6b969a3a79e8c18dbb4ed8c2f0f1e4897a8ed
baolam <lam01662052827@gmail.com>
2025-01-07 13:21:02 +0700

    flip coin with probability

---
+++ #vnoi/atcoder_dp_k/attempt2.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define MAXN 101
+#define MAXK 100002
+
+using namespace std;
+
+int n, k;
+int a[MAXN];
+bool f[MAXK];
+
+int main()
+{
+  memset(f, false, sizeof(f));
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  freopen("TEST.INP", "r", stdin);
+
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++)
+    cin >> a[i];
+
+  for (int i = 1; i <= k; i++)
+  {
+    for (int j = 1; j <= n; j++)
+    {
+      if (i >= a[j] && !f[i - a[j]])
+        f[i] = 1;
+    }
+  }
+
+  if (f[k])
+    cout << "First";
+  else
+    cout << "Second";
+
+  return 0;
+}
